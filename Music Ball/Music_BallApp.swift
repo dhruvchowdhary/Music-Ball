a48e0c94ae3f91f9b12ca8f58f431632534c2b92
dhruvchowdhary <dhruvchowdhary@berkeley.edu>
2023-12-18 19:42:48 -0600

    Initial Commit

---
+++ Music Ball/Music_BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_BallApp.swift
+//  Music Ball
+//
+//  Created by Dhruv Chowdhary on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
